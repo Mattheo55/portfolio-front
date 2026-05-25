7f17e0f8b6961f2964105973742183e4af587318
Matthéo <mattheo55.b@gmail.com>
2026-05-25 18:44:47 +0200

    Add techMock data for React technology

---
+++ src/mocks/tech_mock.ts
@@ -0,0 +1,6 @@
+export const techMock = [
+    {
+        id: "React",
+        content: "J'ai découvert React dans ma quête des techno front-end, aujourd'hui je l'utise avec le framwork NextJS qui permet de faire des sites web performant (Comme se portfolio)"
+    }
+]
